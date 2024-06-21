bb87b6c88cdb03844da1ca8ece587b3148ed8916
gulshan0007 <121476861+gulshan0007@users.noreply.github.com>
2024-06-21 14:38:08 +0530

    fsddsfdvvfdvfdfe

--- _handelsignup.php
+++ _handelsignup.php
@@ -66,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['signupemail'])) {
         //Server settings
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com'; // SMTP server
-        $mail->SMTPAuth = false;
+        $mail->SMTPAuth = true;
         $mail->Username = 'gulshankumar060102@gmail.com'; // SMTP username
         $mail->Password = 'tutraiufsgqfpfjf'; // SMTP password
         $mail->SMTPSecure = 'tls'; // Enable TLS encryption
