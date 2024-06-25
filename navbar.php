89cb164df7d6060872e757d212fc490b22408a94
gulshan0007 <121476861+gulshan0007@users.noreply.github.com>
2024-06-25 23:20:53 +0530

    navbar

--- navbar.php
+++ navbar.php
@@ -1,6 +1,5 @@
 <?php 
-if(!isset($_SESSION))
-{
+if(!isset($_SESSION)) {
     session_start(); 
 }
 ?>
@@ -10,6 +9,9 @@ if(!isset($_SESSION))
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
+<!-- Include Font Awesome for icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 <style>
 .dotted-border {
@@ -31,11 +33,20 @@ if(!isset($_SESSION))
     background-color: white !important;
     border-color: white !important;
     color: white !important;
+    position: relative;
+    padding: 0.25rem 0.75rem;
 }
 
-.navbar-toggler-icon {
-    color: #0a0a0a !important;
-    font-size: 28px !important;
+.navbar-toggler-icon::before {
+    content: '\f0c9'; /* Font Awesome icon for bars */
+    font-family: "Font Awesome 5 Free";
+    font-weight: 900;
+    font-size: 1.25rem;
+    color: #0a0a0a;
+}
+
+.navbar-toggler.collapsed .navbar-toggler-icon::before {
+    content: '\f00d'; /* Font Awesome icon for times (close) */
 }
 
 @media (max-width: 768px) {
@@ -83,48 +94,45 @@ if(!isset($_SESSION))
             <span class="text-white h4 mb-0 ml-2" style="text-decoration: none;"><b>Learners' Space</b></span>
         </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon">
-                <i class="fas fa-bars"></i>
-            </span>
+        <button id="navbarToggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="index.php">Home <i class="fas fa-home"></i></a>
+                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="index.php">Home</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="course.php">Courses <i class="fas fa-book"></i></a>
+                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="course.php">Courses</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="contact.php">Contact <i class="fas fa-envelope"></i></a>
+                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="contact.php">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="faq.php">FAQs <i class="fas fa-question-circle"></i></a>
+                    <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="faq.php">FAQs</a>
                 </li>
 
                 <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) { ?>
                     <li class="nav-item">
-                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="cart.php">Your Cart <i class="fas fa-shopping-cart"></i></a>
+                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="cart.php">Your Cart</a>
                     </li>
                     <li class="nav-item">
-                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="registrations.php">Your Registrations <i class="far fa-calendar-check"></i></a>
+                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="registrations.php">Your Registrations</a>
                     </li>
                     <li class="nav-item">
                         <form method="POST" action="logout.php" class="form-inline my-2 my-lg-0">
-                            <button type="submit" class="btn btn-outline-danger mx-2 my-md-0 my-1 dotted-border" name="Logout">
-                                Logout <i class="fas fa-sign-out-alt"></i></button>
+                            <button type="submit" class="btn btn-outline-danger mx-2 my-md-0 my-1 dotted-border" name="Logout">Logout</button>
                         </form>
                     </li>
                 <?php } else { ?>
                     <li class="nav-item">
-                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="#" data-bs-toggle="modal" data-bs-target="#loginmodal">Login</a>
+                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="#" data-toggle="modal" data-target="#loginmodal">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="#" data-bs-toggle="modal" data-bs-target="#signupmodal">Register</a>
+                        <a class="btn btn-outline-light mx-2 my-md-0 my-1 dotted-border" href="#" data-toggle="modal" data-target="#signupmodal">Register</a>
                     </li>
                 <?php } ?>
             </ul>
@@ -135,3 +143,16 @@ if(!isset($_SESSION))
 <!-- Include login and signup modals here -->
 <?php include 'loginmodal.php'; ?>
 <?php include 'signupmodal.php'; ?>
+
+<script>
+    // Track toggle state
+    var navbarCollapsed = true;
+
+    // Function to reload the page on second toggle click
+    $('#navbarToggler').click(function() {
+        if (!navbarCollapsed) {
+            location.reload(); // Reload page only if previously expanded
+        }
+        navbarCollapsed = !navbarCollapsed;
+    });
+</script>
